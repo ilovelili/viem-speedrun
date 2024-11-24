750b9c72ed2a449cf2593b3114f9bfc4fda2d8d8
ilovelili <route666@live.cn>
2024-11-24 19:16:31 +0900

    part one

---
+++ src/1-account.ts
@@ -0,0 +1,31 @@
+import dotenv from "dotenv";
+import { Address, createPublicClient, formatEther, http } from "viem";
+import { privateKeyToAccount } from "viem/accounts";
+import { arbitrumSepolia } from "viem/chains";
+
+dotenv.config();
+
+const privateKey = process.env.PRIVATE_KEY;
+if (!privateKey) {
+  throw new Error("PRIVATE_KEY is required");
+}
+
+const account = privateKeyToAccount(privateKey as Address);
+console.log(`account address: ${account.address}`);
+
+// IIFE
+(async () => {
+  const client = createPublicClient({
+    chain: arbitrumSepolia,
+    transport: http(process.env.API_URL),
+  });
+
+  const balance = await client.getBalance({ address: account.address });
+
+  // formatEther wei to ether
+  // parseEther ether to wei
+  console.log(formatEther(balance));
+
+  const nonce = await client.getTransactionCount({ address: account.address });
+  console.log(nonce);
+})();
